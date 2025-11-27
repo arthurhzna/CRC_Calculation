67e55038653a8291124222475e7c720bde899583
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-11-27 17:18:19 +0700

    Create inv2.c

---
+++ inv2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define PRESET_VALUE 0xFFFF
+#define POLYNOMIAL   0x8408
+
+unsigned int uiCrc16Cal(unsigned char const *pucY, unsigned char ucX)
+{
+    unsigned char ucI, ucJ;
+    unsigned short int uiCrcValue = PRESET_VALUE;
+
+    for (ucI = 0; ucI < ucX; ucI++)
+    {
+        uiCrcValue ^= *(pucY + ucI);
+        for (ucJ = 0; ucJ < 8; ucJ++)
+        {
+            if (uiCrcValue & 0x0001)
+                uiCrcValue = (uiCrcValue >> 1) ^ POLYNOMIAL;
+            else
+                uiCrcValue >>= 1;
+        }
+    }
+    return uiCrcValue;
+}
+
+int main(void)
+{
+    // Frame INVENTORY-G2 tanpa CRC
+    unsigned char frame[] = {
+        0x10, 0x00, 0x01, 0x04, 0x01, 0x02,
+        0x00, 0x02, 0x04, 0x02, 0x00, 0x00,
+        0x00, 0x80, 0x0A
+    };
+
+
+    // Hitung CRC untuk SELURUH frame di atas
+    unsigned int crc = uiCrc16Cal(frame, sizeof(frame));
+    unsigned char crcLSB = crc & 0xFF;
+    unsigned char crcMSB = (crc >> 8) & 0xFF;
+
+    // Print full frame
+    printf("Final Frame:\n");
+    for (int i = 0; i < sizeof(frame); i++)
+    {
+        printf("%02X ", frame[i]);
+    }
+    printf("%02X %02X\n", crcLSB, crcMSB);
+
+    return 0;
+}
